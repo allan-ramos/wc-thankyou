c3de89d3bfb41660bbe3b52f53a44d3b9fbbe19e
allan-ramos <76536015+allan-ramos@users.noreply.github.com>
2020-12-30 16:00:09 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,20 +6,7 @@ function custom_thankyou() {
  
  	echo '<h2>¡<b>Gracias!</b></h2>
 <h4><b>Tu pedido ha sido procesado.</b></h4>
-<h4><b>Recibirás una llamada por parte de nuestros agentes. </b></4>
-<style>
-.entry-content p {
-    -ms-word-wrap: break-word;
-    word-wrap: break-word;
-    hyphens: none;
-    display: none !important;
-}
-
-.woocommerce ul.order_details {
-    margin: 0 0 3em;
-    list-style: none;
-    display: none !important;
-}</style>';
+<h4><b>Recibirás una llamada por parte de nuestros agentes. </b></4>';
  
 }
 
